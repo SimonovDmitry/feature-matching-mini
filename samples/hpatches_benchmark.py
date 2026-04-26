b65b2977f0662b6cc4bec2d718c70e33cfe77887
SimonovDima <fdjgfdj722@gmail.com>
2026-04-26 18:12:35 +0300

    switch to hpatches-sequences-release for full pipeline mAP evaluation

--- samples/hpatches_benchmark.py
+++ samples/hpatches_benchmark.py
@@ -1,46 +1,19 @@
 import argparse
 import sys
 import logging
-import numpy as np
-import cv2 as cv
 from pathlib import Path
 
-from src.algorithms import DNN_DESCRIPTORS
+from src.detectors import Detector
 from src.descriptors import Descriptor
-from src.super_point import SuperPoint
-from src.preprocessor import Preprocessor
+from src.matchers import Matcher, OpenCVMatcher
+from src.feature_matcher import FeatureMatcherCV2
 
-from samples.hpatches_utils import HPatchesTask, HPatchesDataManager
+from samples.utils import build_config
+from samples.hpatches_utils import HPatchesDataManager, HPatchesTask
+
 
 logging.basicConfig(level=logging.INFO, format='[ %(levelname)s ] %(message)s')
 logger = logging.getLogger("hpatches_benchmark")
-
-
-def build_descriptor_config(args):
-    config = dict()
-    if (args.device is not None) and (args.descriptor in DNN_DESCRIPTORS):
-        config['device'] = args.device
-    if args.des_nfeatures is not None:
-        config['nfeatures'] = args.des_nfeatures
-    if args.des_threshold is not None:
-        config['threshold'] = args.des_threshold
-    if args.des_scale is not None:
-        config['scale_factor'] = args.des_scale
-    return config
-
-
-def build_preprocessor_config(args):
-    config = dict()
-    if args.device is not None:
-        config['device'] = args.device
-    return config
-
-
-def build_config(args):
-    return {
-            'descriptor': build_descriptor_config(args),
-            'preprocessor': build_preprocessor_config(args),
-           }
 
 
 def parser():
@@ -48,21 +21,38 @@ def parser():
         description="HPatches Benchmark Pipeline",
         formatter_class=argparse.ArgumentDefaultsHelpFormatter)
 
+    available_detectors = list(Detector._METHODS.keys())
     available_descriptors = list(Descriptor._METHODS.keys())
+    available_matchers = list(Matcher._METHODS.keys())
+    available_matchers_modes = list(OpenCVMatcher._MODE)
     available_task = list(HPatchesTask._TASKS.keys())
     available_devices = ['cpu', 'cuda', 'mps']
 
+    arg_parser.add_argument('-det', '--detector', type=str, default='sift',
+                            choices=available_detectors, help='Detector algorithm')
     arg_parser.add_argument('-des', '--descriptor', type=str, default='sift',
                             choices=available_descriptors, help='Descriptor algorithm')
-    arg_parser.add_argument('-t', '--task', type=str, default='matching',
-                            choices=available_task, help='Descriptor algorithm')
+    arg_parser.add_argument('-mat', '--matcher', type=str, default='bf',
+                            choices=available_matchers, help='Matching algorithm')
 
     arg_parser.add_argument('-d', '--device', type=str, default=None,
                             choices=available_devices, help='The device on which the script will be run')
+    arg_parser.add_argument('-t', '--task', type=str, default='matching',
+                            choices=available_task, help='Descriptor algorithm')
     arg_parser.add_argument('-p', '--path', type=Path, required=True,
                             help='Path to hpatches-release folder')
     arg_parser.add_argument('-n', '--num-scenes', type=int, default=None,
                             help='Number of scenes to process (default: all)')
+    arg_parser.add_argument('-pt', '--pixel-threshold', type=float, default=5.0,
+                           help='Pixel threshold for homography verification')
+
+    det_group = arg_parser.add_argument_group('Detector config')
+    det_group.add_argument('-dn', '--det-nfeatures', type=int, default=None,
+                           help='Max number of features to detect')
+    det_group.add_argument('-do', '--det-noctave', type=int, default=None,
+                           help='Number of octave layers')
+    det_group.add_argument('-dt', '--det-threshold', type=float, default=None,
+                           help='Detection threshold')
 
     des_group = arg_parser.add_argument_group('Descriptor config')
     des_group.add_argument('-dsen', '--des-nfeatures', type=int, default=None,
@@ -71,7 +61,16 @@ def parser():
                            help='Descriptor threshold')
     des_group.add_argument('-dss', '--des-scale', type=float, default=None,
                            help='Scale factor')
+
+    mat_group = arg_parser.add_argument_group('Matcher config')
+    mat_group.add_argument('-mat_m', '--matcher_mode', type=str, default='simple',
+                           choices=available_matchers_modes, help='Matching mode')
+    mat_group.add_argument('-mr', '--mat-ratio', type=float, default=None,
+                           help='Ratio threshold for KNN')
+    mat_group.add_argument('-mc', '--mat-cross-check', action='store_true', default=None,
+                           help='Enable cross-check for BF matcher')
     return arg_parser.parse_args()
+
 
 
 def main():
@@ -87,44 +86,30 @@ def main():
         config = build_config(args)
         logger.info(f"Config: {config}")
 
+        feature_matcher = FeatureMatcherCV2(detector=args.detector, descriptor=args.descriptor,
+                                            matcher=args.matcher, logger=logger, config=config)
         dm = HPatchesDataManager(raw_data_path=args.path, logger=logger)
-        descriptor = Descriptor.create(descriptor_name=args.descriptor, logger=logger, config=config['descriptor'])
-        preprocessor = Preprocessor(config=config['preprocessor'], logger=logger)
+        dataset = dm.load_dataset(num_scenes=args.num_scenes)
+        matching_data = {scene: {} for scene in dataset.keys()}
 
-        full_dataset = dm.load_dataset(num_scenes=args.num_scenes)
+        for scene_name, data in dataset.items():
+            img_ref = data['ref_img']
 
-        fake_kp = [cv.KeyPoint(32, 32, 31)]
-        NORM_MAP = { cv.NORM_L2: 'L2', cv.NORM_L1: 'L1'}
-        dist_name = NORM_MAP.get(descriptor.default_norm, 'L2')
-        all_descriptors = {'distance': dist_name}
+            for i, target in data['targets'].items():
+                img_tgt = target['image']
+                H = target['H']
 
-        logger.info("Computing descriptors for all scenes")
-        for scene_name, scene_data in full_dataset.items():
-            scene_descr = {}
+                features_ref, features_tgt, correspondences = feature_matcher.match(img_ref, img_tgt)
+                matching_data[scene_name][i] = {
+                    'kp_ref': features_ref['kp'],
+                    'kp_tgt': features_tgt['kp'],
+                    'matches': correspondences['matches'],
+                    'H': H
+                }
 
-            for img_type, patches_batch in scene_data.items():
-                list_of_descr = []
-
-                for patch in patches_batch:
-
-                    features = {'kp': fake_kp, 'des': None}
-                    features = descriptor.compute(patch, features)
-                    features = preprocessor.prepare_features(from_algo=args.descriptor, to_algo='opencv', features=features)
-
-                    if features['des'] is None:
-                        pass
-
-                    list_of_descr.append(features['des'])
-
-                scene_descr[img_type] = np.vstack(list_of_descr)
-
-            all_descriptors[scene_name] = scene_descr
-
-
-        logger.info(f"Running {args.task} Task evaluation")
-        task = HPatchesTask.create(args.task, logger)
-        results = task.eval_task(all_descriptors, {'test': list(full_dataset.keys())})
-        task.report_metrics(results, args.task)
+        task = HPatchesTask.create(task_name=args.task, logger=logger, pixel_threshold=args.pixel_threshold)
+        results = task.eval_task(matching_data, list(dataset.keys()))
+        task.report_metrics(results, "End-to-End Pipeline")
 
         logger.info("Pipeline finished successfully")
         return 0
@@ -136,4 +121,3 @@ def main():
 
 if __name__ == '__main__':
     sys.exit(main() or 0)
-
--- samples/hpatches_utils.py
+++ samples/hpatches_utils.py
@@ -1,41 +1,17 @@
 import cv2 as cv
 import numpy as np
-from scipy import spatial
 import pandas as pd
-from collections import defaultdict
 from abc import ABC, abstractmethod
 from pathlib import Path
-from tqdm import tqdm
 
-try:
-    import torch
-    TORCH_GPU_IS_AVAILABLE = torch.cuda.is_available()
-
-except ImportError:
-    TORCH_GPU_IS_AVAILABLE = False
+from src.image_utils import read_image
 
 
 class HPatchesDataManager:
-    _img_types = ['ref', 'e1', 'e2', 'e3', 'e4', 'e5', 'h1', 'h2', 'h3', 'h4', 'h5']
-    _patch_size = 65
-
     def __init__(self, raw_data_path, logger):
         self._raw_data_path = Path(raw_data_path)
         self._logger = logger
-
-    def load_patches_from_image(self, scene_name, img_type):
-        img_path = self._raw_data_path / scene_name / f"{img_type}.png"
-        if not img_path.exists():
-            return None
-
-        strip = cv.imread(str(img_path), cv.IMREAD_GRAYSCALE)
-        if strip is None: return None
-
-        h, w = strip.shape
-        n_patches = h // self._patch_size
-
-        patches = strip.reshape(n_patches, self._patch_size, self._patch_size)
-        return patches
+        self._img_indices = [2, 3, 4, 5, 6]
 
     def load_dataset(self, num_scenes=None):
         dataset = {}
@@ -47,14 +23,32 @@ class HPatchesDataManager:
         else:
             scenes = all_scenes
 
-        self._logger.info(f"Loading {len(scenes)} scenes")
+        self._logger.info(f"Loading {len(scenes)} scenes for Full Image Matching")
+
         for scene in scenes:
-            scene_data = {}
-            self._logger.info(f"name file {scene}")
-            for t in self._img_types:
-                patches = self.load_patches_from_image(scene, t)
-                if patches is not None:
-                    scene_data[t] = patches
+            scene_dir = self._raw_data_path / scene
+            self._logger.info(f"Loading {scene_dir}")
+
+            ref_path = scene_dir / f"1.ppm"
+            img_ref = read_image(ref_path)
+
+            if img_ref is None:
+                self._logger.warning(f"Could not read reference image in {scene}")
+                continue
+
+            scene_data = {'ref_img': img_ref, 'targets': {}}
+
+            for i in self._img_indices:
+                target_path = scene_dir / f"{i}.ppm"
+                img_target = read_image(target_path)
+
+                h_path = scene_dir / f"H_1_{i}"
+                if img_target is not None and h_path.exists():
+                    H = np.loadtxt(str(h_path))
+                    scene_data['targets'][i] = {
+                        'image': img_target,
+                        f'H': H
+                    }
 
             dataset[scene] = scene_data
 
@@ -63,13 +57,6 @@ class HPatchesDataManager:
 
 class HPatchesTask(ABC):
     _TASKS = {}
-    _tp = ['e', 'h', 't']
-    _id2t = {0: {'e': 'ref', 'h': 'ref', 't': 'ref'},
-             1: {'e': 'e1', 'h': 'h1', 't': 't1'},
-             2: {'e': 'e2', 'h': 'h2', 't': 't2'},
-             3: {'e': 'e3', 'h': 'h3', 't': 't3'},
-             4: {'e': 'e4', 'h': 'h4', 't': 't4'},
-             5: {'e': 'e5', 'h': 'h5', 't': 't5'}}
 
     def __init__(self, logger):
         self._logger = logger
@@ -83,11 +70,11 @@ class HPatchesTask(ABC):
                 HPatchesTask._TASKS[key] = cls
 
     @classmethod
-    def create(cls, task_name, logger):
+    def create(cls, task_name, logger, pixel_threshold=5.0):
         task_class = cls._TASKS.get(task_name.lower())
         if not task_class:
             raise ValueError(f"Unknown task: {task_name}. Available: {list(cls._TASKS.keys())}")
-        return task_class(logger)
+        return task_class(logger, pixel_threshold)
 
     @abstractmethod
     def eval_task(self, descriptors, split):
@@ -95,8 +82,9 @@ class HPatchesTask(ABC):
 
     def report_metrics(self, results, task_name=""):
         rows = []
+
         for scene, res in results.items():
-            aps = [m['ap'] for t in res.values() for m in t.values() if 'ap' in m]
+            aps = [metrics['ap'] for metrics in res.values() if 'ap' in metrics]
             if aps:
                 rows.append({'Scene': scene, 'mAP': np.mean(aps)})
 
@@ -105,8 +93,8 @@ class HPatchesTask(ABC):
             return
 
         df = pd.DataFrame(rows).sort_values('mAP', ascending=False)
-        self._logger.info(f" {task_name.upper()} RESULTS ")
-        self._logger.info(df.to_string(index=False))
+        self._logger.info(f"--- {task_name.upper()} RESULTS ---")
+        self._logger.info("\n" + df.to_string(index=False))
         self._logger.info(f"Mean Total AP: {df['mAP'].mean():.4f}")
 
     @classmethod
@@ -141,86 +129,43 @@ class HPatchesTask(ABC):
         precision = np.maximum(tp, small) / np.maximum(tp + fp, small)
         return precision, recall, np.trapezoid(precision, recall)
 
-    @classmethod
-    def _roc(cls, scores, labels, numpos=None):
-        [tp, fp, p, n, perm] = cls._tpfp(scores, labels, numpos)
-
-        small = 1e-10
-        tpr = tp / float(np.maximum(p, small))
-        fpr = fp / float(np.maximum(n, small))
-        return fpr, tpr, np.trapezoid(tpr, fpr)
-
-    @classmethod
-    def _seqs_lengths(cls, seqs):
-        N = {}
-        for seq in seqs:
-            N[seq] = seqs[seq].N
-        return N
-
-    @classmethod
-    def _dist_matrix(cls, D1, D2, distance):
-        if distance == 'L2':
-            if TORCH_GPU_IS_AVAILABLE:
-                import torch
-                with torch.no_grad():
-
-                    D = torch.cdist(torch.from_numpy(D1).cuda().float(),
-                                    torch.from_numpy(D2).cuda().float(),
-                                    p=2).detach().cpu().numpy()
-            else:
-                D = spatial.distance.cdist(D1, D2, 'euclidean')
-        elif distance == 'L1':
-            if TORCH_GPU_IS_AVAILABLE:
-                import torch
-                with torch.no_grad():
-                    D = torch.cdist(torch.from_numpy(D1).cuda(),
-                                    torch.from_numpy(D2).cuda(),
-                                    p=1).detach().cpu().numpy()
-            else:
-                D = spatial.distance.cdist(D1, D2, 'cityblock')
-        elif distance == 'masked_L1':
-            [desc1, masks1] = np.split(D1, 2, axis=1)
-            [desc2, masks2] = np.split(D2, 2, axis=1)
-            D = spatial.distance.cdist(desc1 * masks1, desc2 * masks2, 'cityblock')
-        else:
-            raise ValueError('Unknown distance - valid options are |L2|L1|masked_L1|')
-        return D
-
 
 class MatchingTask(HPatchesTask):
-    def __init__(self, logger):
+    def __init__(self, logger, pixel_threshold=5.0):
         super().__init__(logger)
+        self.pixel_threshold = pixel_threshold
 
-    def eval_task(self, descr, split):
-        self._logger.info(f'Evaluating task matching')
+    def eval_task(self, matching_data, split):
+        self._logger.info(f'Evaluating Full Pipeline (mAP) via Homography')
+        results = {seq: {} for seq in split}
 
-        results = defaultdict(lambda: defaultdict(lambda: defaultdict(dict)))
-        pbar = tqdm(split['test'])
-        for seq in pbar:
-            d_ref = descr[seq].get('ref')
-            if d_ref is None:
-                continue
+        for seq in split:
+            if seq not in matching_data: continue
 
-            gt_l = np.arange(d_ref.shape[0])
-            for t in self._tp:
-                for i in range(1, 6):
-                    key = t + str(i)
+            for i in range(2, 7):
+                data = matching_data[seq].get(i)
+                if not data or not data['matches']:
+                    continue
 
-                    if key not in descr[seq]:
-                        continue
+                kp_ref = data['kp_ref']
+                kp_tgt = data['kp_tgt']
+                matches = data['matches']
+                H = data['H']
 
-                    d = descr[seq][key]
-                    D = self._dist_matrix(d_ref, d, descr['distance'])
-                    idx = np.argmin(D, axis=1)
-                    m_l = np.equal(idx, gt_l)
-                    results[seq][t][i]['sr'] = np.count_nonzero(m_l) / float(m_l.shape[0])
-                    m_d = D[gt_l, idx]
-                    pr, rc, ap = self._pr(-m_d, m_l, numpos=m_l.shape[0])
-                    results[seq][t][i]['ap'] = ap
-                    results[seq][t][i]['pr'] = pr
-                    results[seq][t][i]['rc'] = rc
+                pts_ref = np.array([kp_ref[m.queryIdx].pt for m in matches], dtype=np.float32).reshape(-1, 1, 2)
+                pts_tgt_pred = np.array([kp_tgt[m.trainIdx].pt for m in matches], dtype=np.float32).reshape(-1, 1, 2)
+                pts_tgt_gt = cv.perspectiveTransform(pts_ref, H)
 
-        self._logger.info(f"Matching task finished")
+                distances = np.linalg.norm(pts_tgt_pred - pts_tgt_gt, axis=2).flatten()
+                labels = (distances <= self.pixel_threshold).astype(int)
+
+                scores = np.array([-m.distance for m in matches])
+                precision, recall, ap = self._pr(scores, labels, numpos=len(kp_ref))
+
+                results[seq][i] = {
+                    'ap': ap,
+                    'precision': precision[-1] if len(precision) > 0 else 0,
+                    'recall': recall[-1] if len(recall) > 0 else 0
+                }
+
         return results
-
-
